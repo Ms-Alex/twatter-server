fa9bea3ef45a9c4130559ca14a7bd1dc17faf395
Ms-Alex <ms.hernandeza1@gmail.com>
2018-10-15 15:21:25 -0400

    fix typo in error handler

--- handlers/error.js
+++ handlers/error.js
@@ -2,9 +2,9 @@
 function errorHandler(err, req, res, next) {
     return res.status(err.status || 500).json({
         error: {
-            message: error.message || "Oops! Something went wrong."
+            message: err.message || "Oops! Something went wrong."
         }
-    })
-};
+    });
+}
 
 module.exports = errorHandler;
