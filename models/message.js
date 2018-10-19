7216e1126faf52e47aa2990cb5efed173dcce1c5
Ms-Alex <ms.hernandeza1@gmail.com>
2018-10-18 21:34:55 -0400

    add mongoose middleware to message schema to make sure message is also removed from users mssgs list

--- models/message.js
+++ models/message.js
@@ -13,6 +13,23 @@ const messageSchema = new mongoose.Schema({
     }
 });
 
-const Message = mongoose.model("Message", messageSchema);
+//assure that message record is not only deleted from message table, but user table too
+messageSchema.pre("remove", async function (next) {
+    try {
+        // find a user
+        let user = await User.findById(this.user);
+        // remove id of message from their message list
+        user.messages.remove(this.id)
+        // save that user
+        await user.save();
+        return next();
+
+    } catch (err) {
+        return next(err);
+    }
+
+});
+
+const Message = mongoose.model('Message', messageSchema);
 
 module.exports = Message;
