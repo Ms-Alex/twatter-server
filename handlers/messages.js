c3b1038dcb1e50c62e9100ccc944f81157e66ecd
Ms-Alex <ms.hernandeza1@gmail.com>
2018-10-19 21:28:11 -0400

    create GET message handler

--- handlers/messages.js
+++ handlers/messages.js
@@ -23,6 +23,17 @@ exports.createMessage = async function(req, res, next) {
     }
 };
 
-exports.getMessage = async function(req, res, next) {};
+exports.getMessage = async function(req, res, next) {
+    try {
+        let message = await db.Message.find(req.params.message.id);
+        return res.status(200).json(message);
+        
+    } catch (err) {
+        return next(err);
+    }
 
-exports.deleteMessage = async function(req, res, next) {};
+};
+
+exports.deleteMessage = async function(req, res, next) {
+    
+};
